ecac1dfbc984b4e364b8737a027c75b80a474b67
Shantanu Gupta <99300527+Shantanugupta43@users.noreply.github.com>
2022-11-20 03:33:53 +0000

    App Created

--- App.js
+++ App.js
@@ -1,14 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useFonts } from 'expo-font'; //importing hook useFonts
+import { LinearGradient } from 'expo-linear-gradient';
+import AppLoading from 'expo-app-loading';
+import { Audio } from 'expo-av';
+
+
+
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync( require('./assets/Smile.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+
+
+
+  const fontsLoad = useFonts({
+    dance: require('./assets/fonts/DancingScript-VariableFont_wght.ttf'),
+  });
+
+  if (!fontsLoad) {
+    return (
+      <AppLoading />
+    )
+  }
+
+
+
+
+
+  const [showValue, setShowValue] = useState(true);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient style={styles.container} colors={["#2193b0","#753a88"]} start={{x:Math.random(0, 0.5), y:Math.random(0,0.5)}} end={{x:Math.random(0,0.8), y:Math.random(0,0.8)}}>
+        <Text style={styles.header}>Satisfying Gradient Game</Text>
+        <Text style={styles.areabody}> Tap the change button in order to change different gradient forms. Button will keep changing its position as you tap. Challenge involves tapping accurately on button each time. Have fun!</Text>
+        <Text style={styles.areabody}> Play the sound and tap on the beat for tougher challenge.</Text>
+        <View style={styles.button}>
+        <Button title="Change" color="#030303" onPress={() => setShowValue(!showValue)} />
+        {showValue? <Text style={{color: 'white', marginBottom: 20}}>Tap faster! Good Luck! </Text> : null}
+        </View>
+        <View style={styles.buttontwo}>
+        <Button title="Play Sound" color="#030303" onPress={playSound} />
+        </View>
+
+
+        </LinearGradient>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +77,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30,
   },
+  header:{
+    fontFamily:'dance',
+    fontSize:26,
+    color:'white'
+    
+  },
+
+  areabody:{
+    marginTop:20,
+    color:'white'
+    
+
+
+  },
+
+  button:{
+    marginTop:20,
+    width:150,
+    
+  },
+
+  buttontwo:{
+    marginTop:20,
+  }
+
 });
+
